58a2782f783045f38416018ff2e757121aac5135
AndersonHJB <cleland1432803776@icloud.com>
2024-11-24 17:15:56 +0800

    V0.0.1

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,21 @@ std::vector<Point> read_csv(const std::string& filename) {
     std::ifstream file(filename);
     std::string line;
     while (std::getline(file, line)) {
+        std::istringstream ss(line);
+        std::string x_str, y_str;
+        if (std::getline(ss, x_str, ',') && std::getline(ss, y_str)) {
+            points.push_back({std::stod(x_str), std::stod(y_str)});
+        }
+    }
+    return points;
+}
 
+int main(int argc, char *argv[]) {
+    std::vector<Point> points = read_csv("data/100000 locations.csv");
+    std::cout << points.size() << std::endl;
+    // std::cout << points.data() << std::endl;
+    std::cout << "Loaded points from CSV:" << std::endl;
+    for (size_t i = 0; i < points.size(); ++i) {
+        std::cout << "Point " << i + 1 << ": (" << points[i].x << ", " << points[i].y << ")" << std::endl;
     }
 }
