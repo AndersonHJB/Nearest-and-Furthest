8fe0f060c4d3afa0ea0ab5162274c32bb96c277a
AndersonHJB <cleland1432803776@icloud.com>
2024-11-24 17:26:40 +0800

    V0.0.2

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@ struct Point {
     double x, y;
 };
 
+// 从 CSV 文件读取点
 std::vector<Point> read_csv(const std::string& filename) {
     std::vector<Point> points;
     std::ifstream file(filename);
@@ -24,12 +25,18 @@ std::vector<Point> read_csv(const std::string& filename) {
     return points;
 }
 
+// 计算标准几何距离
+double standard_distance(const Point& p1, const Point& p2) {
+    return sqrt(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2));
+}
+
 int main(int argc, char *argv[]) {
+
     std::vector<Point> points = read_csv("data/100000 locations.csv");
-    std::cout << points.size() << std::endl;
+    // std::cout << points.size() << std::endl;
     // std::cout << points.data() << std::endl;
-    std::cout << "Loaded points from CSV:" << std::endl;
-    for (size_t i = 0; i < points.size(); ++i) {
-        std::cout << "Point " << i + 1 << ": (" << points[i].x << ", " << points[i].y << ")" << std::endl;
-    }
+    // std::cout << "Loaded points from CSV:" << std::endl;
+    // for (size_t i = 0; i < points.size(); ++i) {
+    //     std::cout << "Point " << i + 1 << ": (" << points[i].x << ", " << points[i].y << ")" << std::endl;
+    // }
 }
